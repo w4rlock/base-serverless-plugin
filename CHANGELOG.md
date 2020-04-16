dd9201bc1e891dfddddcfd3a0121a501f052a7a0
Emanuel Rosal <emanuel.rosa@naranja.com>
2020-04-16 14:25:09 -0300

    fix: getconf

---
+++ CHANGELOG.md
@@ -0,0 +1,24 @@
+# Changelog
+
+All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
+
+### [0.1.1](https://github.com/w4rlock/base-serverless-plugin/compare/0.1.0...0.1.1) (2020-04-03)
+
+
+### Bug Fixes
+
+* remove logger ([bd1f12e](https://github.com/w4rlock/base-serverless-plugin/commit/bd1f12e0afb7e1fc42039723238f498264d793bb))
+
+## [0.1.0](https://github.com/w4rlock/base-serverless-plugin/compare/0.0.2...0.1.0) (2020-04-03)
+
+
+### Features
+
+* rename and docs ([f4a26d2](https://github.com/w4rlock/base-serverless-plugin/commit/f4a26d287a91df3c9fbc0a875065c3d5162241e5))
+
+### 0.0.2 (2020-04-02)
+
+
+### Features
+
+* first commit ([6ceed9e](https://github.com/w4rlock/base-serverless-plugin/commit/6ceed9e95a564f8c2c2a99680a420f9f9476a07e))
---
+++ index.js
@@ -0,0 +1,84 @@
+const _ = require('lodash');
+const R = require('ramda');
+
+class BasePlugin {
+  /**
+   * Base Plugin Constructor
+   *
+   * @param {object} serverless Serverless instance
+   * @param {object} options object
+   * @param {string} logPreffix console log preffixer
+   * @param {string} usrCustomConfig Serverless yml root key user config
+   */
+  constructor(serverless, options, logPreffix, usrCustomConfig) {
+    this.options = options;
+    this.serverless = serverless;
+    this.logPreffix = logPreffix;
+    this.usrCustomConfig = usrCustomConfig;
+  }
+
+  /**
+   * Check plugin for disabled state
+   *
+   * @returns {boolean} if plugin is disabled based on user config
+   */
+  isPluginDisabled() {
+    const disabled = this.getConf('disabled', false, false);
+    return (_.isBoolean(disabled) && disabled) || disabled === 'true';
+  }
+
+  /**
+   * Log to console
+   * @param {string} entity to log
+   */
+  log(entity) {
+    const str = R.when(R.is(Object), JSON.stringify, entity);
+    this.serverless.cli.log(`${this.logPreffix} ${str}`);
+  }
+
+  /**
+   * Get user config value.. from env,cmd-arg or serverless.yml value
+   *
+   * @param {string} field config field
+   * @param {boolean } required=true launch exception if required and value is missing
+   * @param {object} defaultValue=undefined value by default to return
+   * @returns {object} return value
+   */
+  getConf(field, required = true, defaultValue = undefined) {
+    /* eslint no-underscore-dangle: ["error", { "allow": ["__"] }] */
+    const replaceDotPath = R.replace(/\./g, R.__);
+    const fromEnv = (k) => process.env[k];
+    const fromCmdArg = (k) => this.options[k];
+    const fromYaml = (k) => _.get(this.serverless, `service.custom.${k}`);
+    const checkVal = (v) => v !== undefined && v !== '';
+    const fixVal = (v) => {
+      if (v === 'false') return false;
+      if (v === 'true') return true;
+      return v;
+    };
+
+    const key = `${this.usrCustomConfig}.${field}`;
+
+    let k = replaceDotPath('-')(key);
+    let val = fromCmdArg(k);
+    if (checkVal(val)) return fixVal(val);
+
+    k = replaceDotPath('_')(key);
+    val = fromEnv(k.toUpperCase());
+    if (checkVal(val)) return fixVal(val);
+
+    k = key;
+    val = fromYaml(k);
+    if (checkVal(val)) return fixVal(val);
+
+    if (required && !defaultValue) {
+      throw new Error(
+        `Property value for '${key}' is missing. Please, check your serverless.yml'`
+      );
+    }
+
+    return defaultValue;
+  }
+}
+
+module.exports = BasePlugin;
